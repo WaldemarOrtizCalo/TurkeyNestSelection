7b5f525d85d7da36c7754acfd93855500831e98a
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2021-01-23 19:43:42 -0700

    Added weather station data to Nest

--- 2.Scripts/1.DataPrep.R
+++ 2.Scripts/1.DataPrep.R
@@ -14,7 +14,10 @@
 library(tidyverse)
 library(survival)
 library(survminer)
-library(sp)
+library(sf)
+library(mapview)
+library(nngeo)
+library(foreach)
 
 #      Functions                                                            ####
 
@@ -22,8 +25,24 @@ library(sp)
 #      Data                                                                 ####
 #        [Turkey Data]                                                      ####
 
-Data <- read.csv("1.Data/Final.csv")
-Data_MC <- read.csv("1.Data/Final_MissingCases.csv")
+# Data Upload
+Data <- read.csv("1.Data/Final_MissingCases.csv") %>% 
+  subset(Data, Data$Nest_Type == "Nest") %>% na.omit()
+
+
+# Creating Spatial Object                                         
+
+TurkeySpatial <- st_as_sf(x = Spatial_Data,                         
+                          coords = c("Location_X", "Location_Y"),
+                          crs = "+init=epsg:32615")
+
+# Recoding the Nest Fate to be a categorical characters variable 
+TurkeySpatial$Nest_Fate <- ifelse(TurkeySpatial$Nest_Fate == 1, "Successful","Failed")
+
+# Making Map  
+
+TurkeySpatial %>%
+  mapview(zcol = "Nest_Fate", burst = TRUE, fill = c("green","red") )
 
 #        [Weather Data]                                                     ####
 
@@ -32,17 +51,35 @@ WeatherStations <- read_csv("1.Data/WeatherStations.csv")
 
 # Making into a spatial object
 
-#   Designation of Coordinate Information  
-xy_weather <- WeatherStations[,c("Lat","Lon")] 
+WeatherStationsSpatial <- st_as_sf(x = WeatherStations,                         
+                                   coords = c("Lon", "Lat"),
+                                   crs = "+init=epsg:4326") %>% 
+  st_transform(crs = "+init=epsg:32615")
 
-#   Creating Spatial Object
-WeatherStationsSpatial <- SpatialPointsDataFrame(coords = xy_weather, data = WeatherStations,
-                                        proj4string = CRS("+init=epsg:4326"))
-
-WeatherStationsSpatial <- spTransform(WeatherStationsSpatial,
-                                      CRS = CRS("+init=epsg:32615"))
-
-Map <- WeatherStationsSpatial %>%
-  mapview(burst = TRUE)
+# Checking the Data visually 
+mapview(WeatherStationsSpatial)
 
 ###############################################################################
+#   Calculating Distance Between Nests and Stations                         ####
+
+# Calculating Distance Matrix. Row = Nests and Columns = Weather Stations.
+# The output is the index for the weather station. 
+DistanceMatrix <- st_distance(TurkeySpatial,WeatherStationsSpatial) %>% as.data.frame()
+
+# Finding weather station Indices 
+Indices <- st_nn(TurkeySpatial,WeatherStationsSpatial)
+
+Indices <- do.call(rbind,Indices)
+
+# Turkey Data with the Closest Weather Station name
+WeatherStationTurkey <- foreach(i = 1:length(Indices), .combine = rbind) %do% {
+  Nest <- TurkeySpatial[i,]
+  WeatherStation <- WeatherStationsSpatial[Indices[i],]
+  df <- cbind(Nest,WeatherStation)
+}
+
+###############################################################################
+#   Adding Weather Data                                                     ####
+
+
+
