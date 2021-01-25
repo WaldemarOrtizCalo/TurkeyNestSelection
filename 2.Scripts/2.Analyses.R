627928e67b1220960d4032edb93a8092f4ff19ab
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2021-01-25 15:00:47 -0700

    Bulding Weather Models

--- 2.Scripts/2.Analyses.R
+++ 2.Scripts/2.Analyses.R
@@ -2,29 +2,39 @@
 
 # Author: Waldemar Ortiz-Calo
 
-# Date:2020-10-15 
+# Date:2021-01-25 
 
-# Purpose: This script is designed  to analyze the turkey data to model nest selection
-# and incubation based fate determination.
+# Purpose: 
 
 ###############################################################################
 #   Library / Functions / Data                                              ####
-##  Library                                                                 ####
+
+#      Library                                                              ####
+
 library(tidyverse)
 library(survival)
 library(survminer)
-##  Functions                                                               ####
-##  Data                                                                    ####
+library(stargazer)
+
+#      Functions                                                            ####
+
+#      Data                                                                 ####
+
+#        [Original]                                                         ####
 
 Data <- read.csv("1.Data/Final.csv")
+
 Data_MC <- read.csv("1.Data/Final_MissingCases.csv")
 
-# Data Notes #
-# there are 79 nesting events with 65 unique nesti
+#        [Weather Data]                                                     ####
+
+Data_Weather <- read_csv("1.Data/FinalData.csv")
+
+Data_Weather$Nest_Fate <- if_else(Data_Weather$Nest_Fate == "Successful", 1, 0)
 
 ###############################################################################
-#                         Site Selection Analysis                           ####
-##  Data Prep                                                               ####
+#   Site Selection Analysis (Not Done)                                      ####
+#      Data Prep                                                            ####
 
 # Eliminating Hens with repeat nest
 Data_NestSelection <- subset(Data_MC, Data_MC$Nest_Number == 1) %>% .[,c(1:(ncol(Data_MC)-3))]
@@ -32,7 +42,8 @@ Data_NestSelection <- subset(Data_MC, Data_MC$Nest_Number == 1) %>% .[,c(1:(ncol
 # Changing the Nest type to a binary variable
 Data_NestSelection$Nest_Type <- ifelse(Data_NestSelection$Nest_Type == "Nest",1,0) # 1 = Used ; 0 = Available
 
-##  Variable Prep                                                           ####
+#      Variable Prep                                                        ####
+                                               
 # Dependent Variable
 response <- Data_NestSelection[,'Nest_Type']
 
@@ -55,35 +66,33 @@ Micro12 <- df[,'% SC (1.0-1.8 m)']
 
 
 
+#      Model Sets                                                           ####
+
 # Model 
 
 m1 <- clogit(response ~ Micro01 + strata(strata),data = Data_NestSelection)
 
 summary(m1)
 
-
-##  Model Sets                                                              ####
-
-
 ###############################################################################
-#                          Nest Success Analysis                            ####
-
-
-##  Data Prep                                                               ####
+#   Site Selection Analysis                                                 ####
+#      Data Prep                                                            ####
 
 # Removing Random nest from the data 
-Data_CoxHaz <- subset(Data_MC, Data_MC$Nest_Type == "Nest")
+Data_CoxHaz <- subset(Data_Weather,Data_Weather$Nest_Type == "Nest")
 
 # Making Sure Number of days is a numeric variable 
 Data_CoxHaz$NumberOfDays <- as.numeric(Data_CoxHaz$NumberOfDays)
 Data_CoxHaz$Nest_Fate <- as.numeric(Data_CoxHaz$Nest_Fate)
 
-##  Variable Prep                                                           ####
+#      Variable Prep                                                        ####
 
 # Dependent Variable as a Survival Object 
+
 response <- Surv(Data_CoxHaz$NumberOfDays,Data_CoxHaz$Nest_Fate)
 
-# Independent Variables
+# Independent Variables [Habitat]
+
 HabitatType              <- factor(Data_CoxHaz$Habitat_Type, levels = c("Opening","Edge","Forest"))
 Forest_100m              <- Data_CoxHaz$Amount_Forest_100m
 Herb_100m                <- Data_CoxHaz$Amount_Herb_100m
@@ -112,8 +121,16 @@ Percent_SC1              <- Data_CoxHaz$Percent_ScreeningCover_0_0.5
 Percent_SC2              <- Data_CoxHaz$Percent_ScreeningCover_0.5_1.0
 Percent_SC3              <- Data_CoxHaz$Percent_ScreeningCover_1.0_1.8
 
-##  Model Sets                                                              ####
-### Global Model                                                            ####
+# Independent Variables [Weather]
+PrecipAmount             <- Data_CoxHaz$Total_Precip
+PrecipDays               <- Data_CoxHaz$RainDays
+MeanTemp                 <- Data_CoxHaz$MeanTemp
+MaxTemp                  <- Data_CoxHaz$MaxTemp
+MinTemp                  <- Data_CoxHaz$MinTemp
+
+#      Model Sets                                                           ####
+#        [Global Model]                                                     ####
+
 Global_HazCox <- coxph(response ~ HabitatType + 
                          Forest_100m + Forest_200m + Forest_400m +
                          Herb_100m + Herb_200m + Herb_400m +
@@ -127,15 +144,12 @@ Global_HazCox <- coxph(response ~ HabitatType +
 
 summary(Global_HazCox)
 
-
-
-
-### Habitat Model (Significant)                                             ####
+#        [Habitat Model]  (Significant)                                     ####
 Habitat_HazCox <- coxph(response ~ HabitatType)
 
 summary(Habitat_HazCox)
 
-### Micro Model                                                             ####
+#        [Micro Model]                                                      ####
 Micro_HazCox <- coxph(response ~ BasalArea + ForestEdgeDist + Height_GC +
                         Percent_CanopyClosure + Percent_BareGroundLitter +
                         Percent_Grasses + Percent_Forbs + Percent_WoodyVeg +
@@ -143,7 +157,7 @@ Micro_HazCox <- coxph(response ~ BasalArea + ForestEdgeDist + Height_GC +
 
 summary(Micro_HazCox)
 
-### Macro Model (Somewhat significant)                                      ####
+#        [Macro Model] (Somewhat significant)                               ####
 Macro_HazCox <- coxph(response ~ Forest_100m + Forest_200m + Forest_400m +
                         Herb_100m + Herb_200m + Herb_400m +
                         Crop_100m + Crop_200m + Crop_400m +
@@ -152,13 +166,13 @@ Macro_HazCox <- coxph(response ~ Forest_100m + Forest_200m + Forest_400m +
 
 summary(Macro_HazCox)
 
-### Global 100m Model                                                       ####
+#        [Global 100m Model]                                                ####
 Macro100m_HazCox <- coxph(response ~ Forest_100m + Herb_100m + Crop_100m + 
                             Other_100m + ForestEdge_100m)
 
 summary(Macro100m_HazCox)
 
-### Global 200m Model                                                       ####
+#        [Global 200m Model]                                                ####
 Macro200m_HazCox <- coxph(response ~ Forest_200m + Herb_200m + Crop_200m + 
                             Other_200m + ForestEdge_200m)
 
@@ -166,7 +180,7 @@ summary(Macro200m_HazCox)
 
 
 
-### Global 400m Model                                                       ####
+#        [Global 400m Model]                                                ####
 Macro400m_HazCox <- coxph(response ~ Forest_400m + Herb_400m + Crop_400m + 
                             Other_400m + ForestEdge_400m)
 
@@ -174,169 +188,205 @@ summary(Macro400m_HazCox)
 
 
 
-### Forest 100m Model                                                       ####
+#        [Forest 100m Model]                                                ####
 Forest100m_HazCox <- coxph(response ~ Forest_100m)
 
 summary(Forest100m_HazCox)
 
 
-### Forest 200m Model                                                       ####
+#        [Forest 200m Model]                                                ####
 Forest200m_HazCox <- coxph(response ~ Forest_200m)
 
 summary(Forest200m_HazCox)
 
 
 
-### Forest 400m Model                                                       ####
+#        [Forest 400m Model]                                                ####
 Forest400m_HazCox <- coxph(response ~ Forest_400m)
 
 summary(Forest400m_HazCox)
 
 
 
-### Herb 100m Model                                                         ####
+#        [Herb 100m Model]                                                  ####
 Herb100m_HazCox <- coxph(response ~ Herb_100m)
 
 summary(Herb100m_HazCox)
 
-### Herb 200m Model                                                         ####
+#        [Herb 200m Model]                                                  ####
 Herb200m_HazCox <- coxph(response ~ Herb_200m)
 
 summary(Herb200m_HazCox)
 
 
-### Herb 400m Model                                                         ####
+#        [Herb 400m Model]                                                  ####
 Herb400m_HazCox <- coxph(response ~ Herb_400m)
 
 summary(Herb400m_HazCox)
 
 
-### Crop 100m Model                                                         ####
+#        [Crop 100m Model]                                                  ####
 Crop100m_HazCox <- coxph(response ~ Crop_100m)
 
 summary(Crop100m_HazCox)
 
 
-### Crop 200m Model (Somewhat Significant)                                  ####
+#        [Crop 200m Model] (Somewhat Significant)                           ####
 Crop200m_HazCox <- coxph(response ~ Crop_200m)
 
 summary(Crop200m_HazCox)
 
 
 
-### Crop 400m Model (Somewhat Significant)                                  ####
+#        [Crop 400m Model] (Somewhat Significant)                           ####
 Crop400m_HazCox <- coxph(response ~ Crop_400m)
 
 summary(Crop400m_HazCox)
 
 
 
-### Other 100m Model                                                        ####
+#        [Other 100m Model]                                                 ####
 Other100m_HazCox <- coxph(response ~ Other_100m)
 
 summary(Other100m_HazCox)
 
 
-### Other 200m Model                                                        ####
+#        [Other 200m Model]                                                 ####
 Other200m_HazCox <- coxph(response ~ Other_200m)
 
 summary(Other200m_HazCox)
 
 
 
-### Other 400m Model                                                        ####
+#        [Other 400m Model]                                                 ####
 Other400m_HazCox <- coxph(response ~ Other_400m)
 
 summary(Other400m_HazCox)
 
 
 
-### Forest Edge 100m Model                                                  ####
+#        [Forest Edge 100m Model]                                           ####
 ForestEdge100m_HazCox <- coxph(response ~ ForestEdge_100m)
 
 summary(ForestEdge100m_HazCox)
 
 
-### Forest Edge 200m Model                                                  ####
+#        [Forest Edge 200m Model]                                           ####
 ForestEdge200m_HazCox <- coxph(response ~ ForestEdge_200m)
 
 summary(ForestEdge200m_HazCox)
 
 
 
-### Forest Edge 400m Model                                                  ####
+#        [Forest Edge 400m Model]                                           ####
 ForestEdge400m_HazCox <- coxph(response ~ ForestEdge_400m)
 
 summary(ForestEdge400m_HazCox)
 
 
 
-### Basal Area Model                                                        ####
+#        [Basal Area Model]                                                 ####
 
 BasalArea_HazCox <- coxph(response ~ BasalArea)
 
 summary(BasalArea_HazCox)
 
-### Forest Edge Dist Model                                                  ####
+#        [Forest Edge Dist Model]                                           ####
 
 ForestEdgeDist_HazCox <- coxph(response ~ ForestEdgeDist)
 
 summary(ForestEdgeDist_HazCox)
 
-### Height GC Model                                                         ####
+#        [Height GC Model]                                                  ####
 
 HeightGC_HazCox <- coxph(response ~ Height_GC)
 
 summary(HeightGC_HazCox)
 
-### Percent Canopy Closure Model                                            ####
+#        [Percent Canopy Closure Model]                                     ####
 
 PercentCanopyClosure_HazCox <- coxph(response ~ Percent_CanopyClosure)
 
 summary(PercentCanopyClosure_HazCox)
 
-### Percent Bare Ground Litter Model                                        ####
+#        [Percent Bare Ground Litter Model]                                 ####
 
 PercentBareGroundLitter_HazCox <- coxph(response ~ Percent_BareGroundLitter)
 
 summary(PercentBareGroundLitter_HazCox)
 
-### Percent Grasses Model                                                   ####
+#        [Percent Grasses Model]                                            ####
 
 PercentGrasses_HazCox <- coxph(response ~ Percent_Grasses)
 
 summary(PercentGrasses_HazCox)
 
-### Percent Forbs Model                                                     ####
+#        [Percent Forbs Model]                                              ####
 
 PercentForbs_HazCox <- coxph(response ~ Percent_Forbs)
 
 summary(PercentForbs_HazCox)
 
-### Percent WoodyVeg Model                                                  ####
+#        [Percent WoodyVeg Model]                                           ####
 
 PercentWoodyVeg_HazCox <- coxph(response ~ Percent_WoodyVeg)
 
 summary(PercentWoodyVeg_HazCox)
 
-### Percent SC1 Model                                                       ####
+#        [Percent SC1 Model]                                                ####
 
 PercentSC1_HazCox <- coxph(response ~ Percent_SC1)
 
 summary(PercentSC1_HazCox)
 
-### Percent SC2 Model                                                       ####
+#        [Percent SC2 Model]                                                ####
 
 PercentSC2_HazCox <- coxph(response ~ Percent_SC2)
 
 summary(PercentSC2_HazCox)
 
-### Percent SC3 Model                                                       ####
+#        [Percent SC3 Model]                                                ####
 
 PercentSC3_HazCox <- coxph(response ~ Percent_SC3)
 
 summary(PercentSC3_HazCox)
 
+
+
+
+
+
+
+
+#        [Precipitation Amount Model]                                       ####
+
+MeanPrecip_HazCox <- coxph(response ~ PrecipAmount)
+
+summary(MeanPrecip_HazCox)
+
+#        [Precipitation Days Model]                                         ####
+
+PrecipDays_HazCox <- coxph(response ~ PrecipDays)
+
+summary(PrecipDays_HazCox)
+
+#        [Mean Temp Model]                                                  ####
+
+MeanTemp_HazCox <- coxph(response ~ MeanTemp)
+
+summary(MeanTemp_HazCox)
+
+#        [MaxTemp Model]                                                    ####
+
+MaxTemp_HazCox <- coxph(response ~ MaxTemp)
+
+summary(MaxTemp_HazCox)
+
+#        [MinTemp Model]                                                    ####
+
+MinTemp_HazCox <- coxph(response ~ MinTemp)
+
+summary(MinTemp_HazCox)
 
 
 #   Model Diagnostics                                                       ####
@@ -353,3 +403,5 @@ test.ph <- cox.zph(HazCox)
 ggcoxzph(test.ph)
 
 
+
+###############################################################################
